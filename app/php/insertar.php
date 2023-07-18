849b272579840d505bf5a903bdae0e491a30bc72
Hackuna MSI <oliver885@hotmail.com>
2023-07-17 18:05:22 -0600

    17072023

---
+++ app/php/insertar.php
@@ -0,0 +1,14 @@
+<?php
+
+error_reporting(E_ALL ^ E_DEPRECATED);
+header("Content-Type: text/html; Charset=UTF-8");
+
+$variable1 = 1234;
+$variable2 = "OLIVER";
+
+echo $variable1 . '<br>' . $variable2;
+
+
+
+
+?>
