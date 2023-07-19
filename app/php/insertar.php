7d375b46cdea1776a2feaa88cbdcfefbe1f1fd5c
Dell Mini <oliver885@hotmail.com>
2023-07-18 18:02:12 -0600

    18072023

--- app/php/insertar.php
+++ app/php/insertar.php
@@ -3,12 +3,19 @@
 error_reporting(E_ALL ^ E_DEPRECATED);
 header("Content-Type: text/html; Charset=UTF-8");
 
-$variable1 = 1234;
-$variable2 = "OLIVER";
+// Variables del fomrulario
+$nombre = isset($_POST['nombre']) ? $_POST['nombre'] : '';
+$correo = isset($_POST['correo']) ? $_POST['correo'] : '';
+$password = isset($_POST['password']) ? $_POST['password'] : '';
 
-echo $variable1 . '<br>' . $variable2;
+// Conexion a Data
+$con = new SQLite3("../data/tuvsa.db") or die("Problemas para conectar");
 
+// Consulta a SQL
+$cs = $con -> query("INSERT INTO registro (nombre, correo, password) VALUES ('$nombre', '$correo', '$password')");
 
+echo '<script>alert("Datos Insertados")</script>';
+echo '<script>window.location="../../registro.html"</script>';
 
 
 ?>
